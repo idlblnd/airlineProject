4df6fd43191f361b4ab3e60bb057640f47ed317c
İdil Balandı <idilbalandi@hotmail.com>
2026-03-29 20:46:39 +0300

    fix server start for azure

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-require("dotenv").config();
+const app = require("./app");
 
-const app = require("./app"); // backend app
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🔥 Backend running on http://localhost:${PORT}`);
+  console.log(`🔥 Server running on port ${PORT}`);
 });
