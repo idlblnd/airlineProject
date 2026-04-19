de28d1a5d34f0c5e22646b70e4151fdcab132bec
İdil Balandı <idilbalandi@hotmail.com>
2026-04-19 22:17:00 +0300

    rate limit assigned to 3

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const queryFlightLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 1 gün
-  max: 100,
+  max: 3,
   message: {
     status: "ERROR",
     message: "You can only query flights 3 times per day"
